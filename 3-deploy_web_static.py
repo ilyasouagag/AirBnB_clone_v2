5e7779c922a91ee51a7eba31cfc60d4c90bb70c7
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-08 02:22:32 +0100

    advanced

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -28,14 +28,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
+    """Distributes an archive to a web server."""
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]

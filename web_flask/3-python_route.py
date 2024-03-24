d5586425fd0619e80821c4342f1b0804a0bcac2e
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-24 05:27:22 +0000

    another route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display():
+    """display hello hbnb in the root"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    "display HBNB in /hbnb"
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def message(text):
+    "display text message in /c/<text>"
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def ano_message(text):
+    "display text message in python/<text>"
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")

eae7331f1626f71d5634de7dd0d7a3e93c167ad0
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-24 18:55:24 +0000

    render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -34,6 +34,9 @@ def number(n):
     "display the number if it's integer"
     return "{} is a number".format(n)
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n):
+    return render_template('5-number.html',n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
